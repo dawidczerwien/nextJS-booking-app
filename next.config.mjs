c8d398f37ffbe94de2ee189819e193c057bbb9a2
dawidczerwien <dawidcze8@gmail.com>
2024-03-08 19:55:34 +0100

    fixed depreciaced config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-    images: {
-        domains: ['res.cloudinary.com']
-    }
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [{ hostname: 'res.cloudinary.com' }],
+  },
 };
 
 export default nextConfig;
