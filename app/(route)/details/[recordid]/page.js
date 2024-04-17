9043b7d029304c0d8fd1ab1520897fe2f586f09f
dawidczerwien <dawidcze8@gmail.com>
2024-04-17 22:16:46 +0200

    added doctor sugestion list to details page

--- app/(route)/details/[recordid]/page.js
+++ app/(route)/details/[recordid]/page.js
@@ -2,6 +2,7 @@
 import GlobalApi from '@/app/_utils/GlobalApi';
 import React, { useEffect, useState } from 'react';
 import DoctorDetail from '../_components/DoctorDetail';
+import DoctorSuggestionList from '../_components/DoctorSuggestionList';
 
 const Details = ({ params }) => {
   const [doctor, setDoctor] = useState();
@@ -12,17 +13,19 @@ const Details = ({ params }) => {
   const getDoctorById = () => {
     GlobalApi.getDoctorById(params.recordid).then((res) => {
       setDoctor(res.data.data);
-      console.log(res.data.data);
     });
   };
   return (
-    <div className='p-5 md:px-20'>
+    <div className='p-5 md:px-10'>
       <h2 className='font-bold text-[22px]'>Details</h2>
-      <div className='grid sm:grid-cols-1 md:grid-cols-4'>
+      <div className='grid sm:grid-cols-1 lg:grid-cols-4'>
         <div className='col-span-3'>
-        {doctor && <DoctorDetail doctor={doctor}/>}
+          {doctor && <DoctorDetail doctor={doctor} />}
         </div>
-        <div></div>
+        {/* Doctor Suggestion */}
+        <div>
+          <DoctorSuggestionList />
+        </div>
       </div>
     </div>
   );
